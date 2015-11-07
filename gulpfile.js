29438ccd2c7bdf7a84204d3cacb14129ce361e5a
Axel Chalon <axel.chalon@hetic.net>
2015-11-07 11:59:04 +0100

    - Désactivé nunjucksRender (pas besoin pour l'instant ni probablement plus tard, je galère déjà à mettre en place le workflow de mon côté, pas besoin de perdre plus de temps sur ça) - Pour l'instant, les templates sont côté src/templates/ et sont copiés vers dist/templates/ au cas où on voudra précompiler (concaténer) les templates plus tard - dist/ contient les fichiers statiques (on peut travailler dessus directement) et les fichiers générés par gulp (templates/ et css/app.css)

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp        = require('gulp');
-var nunjucksRender = require('gulp-nunjucks-render');
+// var nunjucksRender = require('gulp-nunjucks-render');
 var plumber = require('gulp-plumber');
 var sass = require('gulp-sass');
 
@@ -8,25 +8,25 @@ var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
 gulp.task('scss', function () {
-  gulp.src('client/scss/app.scss')
+  gulp.src('src/scss/app.scss')
     .pipe(plumber())
     .pipe(sass())
-    .pipe(gulp.dest('app/css/'))
+    .pipe(gulp.dest('dist/css/'))
     .pipe(browserSync.stream());
 });
 
 gulp.task('nunjucks', function () { 
-  nunjucksRender.nunjucks.configure(['client/templates/']); 
-  return gulp.src('client/templates/*.html')
+  // nunjucksRender.nunjucks.configure(['src/templates/']); 
+  return gulp.src('src/templates/*.html')
     .pipe(plumber())
-    .pipe(nunjucksRender())
-    .pipe(gulp.dest('app'))
+    // .pipe(nunjucksRender()) pas besoin d'injecter les variables pour l'instant (en vrai je pense qu'on n'en aura pas besoin, suffit de ajax des json, à voir)
+    .pipe(gulp.dest('dist/templates'))
     .pipe(reload({stream: true}));
 });
 
 gulp.task('watch', function () {
   browserSync.init({ server: { baseDir: '' } });
 
-  gulp.watch("client/templates/*.html", ['nunjucks']);
-  gulp.watch("client/scss/**/*.scss", ['scss']);
+  gulp.watch("src/templates/*.html", ['nunjucks']);
+  gulp.watch("src/scss/**/*.scss", ['scss']);
 });
