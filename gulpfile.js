e511d5a73d3310da5781bfec29bbccf593038d1d
Axel Chalon <axel.chalon@hetic.net>
2015-11-07 12:21:00 +0100

    OK cf clarification fb

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,40 @@
-var gulp        = require('gulp');
-// var nunjucksRender = require('gulp-nunjucks-render');
+var gulp = require('gulp');
+var nunjucksRender = require('gulp-nunjucks-render');
 var plumber = require('gulp-plumber');
 var sass = require('gulp-sass');
+var del = require('del');
 
 // BrowserSync
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
 gulp.task('scss', function () {
-  gulp.src('src/scss/app.scss')
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(gulp.dest('dist/css/'))
-    .pipe(browserSync.stream());
+	gulp.src('src/scss/app.scss')
+		.pipe(plumber())
+		.pipe(sass())
+		.pipe(gulp.dest('dist/css/'))
+		.pipe(browserSync.stream());
 });
 
-gulp.task('nunjucks', function () { 
-  // nunjucksRender.nunjucks.configure(['src/templates/']); 
-  return gulp.src('src/templates/*.html')
-    .pipe(plumber())
-    // .pipe(nunjucksRender()) pas besoin d'injecter les variables pour l'instant (en vrai je pense qu'on n'en aura pas besoin, suffit de ajax des json, à voir)
-    .pipe(gulp.dest('dist/templates'))
-    .pipe(reload({stream: true}));
+gulp.task('nunjucks', function () {
+	nunjucksRender.nunjucks.configure(['src/templates/']);
+	return gulp.src('src/templates/*.html')
+		.pipe(del('dist/*.html'))
+		.pipe(plumber())
+		.pipe(nunjucksRender())
+		.pipe(gulp.dest('dist/'))
+		.pipe(reload({
+			stream: true
+		}));
 });
 
-gulp.task('watch', function () {
-  browserSync.init({ server: { baseDir: 'dist' } });
+gulp.task('watch', function ()  {
+	browserSync.init({
+		server: {
+			baseDir: 'dist'
+		}
+	});
 
-  gulp.watch("src/templates/*.html", ['nunjucks']);
-  gulp.watch("src/scss/**/*.scss", ['scss']);
+	gulp.watch("src/templates/*.html", ['nunjucks']);
+	gulp.watch("src/scss/**/*.scss", ['scss']);
 });
