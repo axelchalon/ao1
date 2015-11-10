d43d0dca7d6cef4e0754d5d5e10d70cff247d263
baloran <contact@baloran.fr>
2015-11-10 14:35:29 +0100

    add search.html + update

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,4 +1,4 @@
 $('.menu-burger-icon').on('click', function (e) {
-  $('.container-menu-burger,.hero,.container-total,.header').toggleClass('open');
+  $('.container-menu-burger,.hero,.container-total,.header,.footer').toggleClass('open');
   return false;
 });
